77463a26c92492b4347169523bdf80766cdfe988
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-05-02 11:59:46 +0530

    admin module with enquirylist component

---
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/admin/admin.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { EnquirylistComponent } from './pages/enquirylist/enquirylist.component';
+
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    EnquirylistComponent
+  ],
+  imports: [
+    CommonModule,
+    AdminRoutingModule
+  ]
+})
+export class AdminModule { }
---
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/admin/pages/enquirylist/enquirylist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enquirylist',
+  templateUrl: './enquirylist.component.html',
+  styleUrl: './enquirylist.component.css'
+})
+export class EnquirylistComponent {
+
+}
