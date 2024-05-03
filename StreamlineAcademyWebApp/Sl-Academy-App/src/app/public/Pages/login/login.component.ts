bf7f6d2282643ae918ffeb207e4c2ee27aad239e
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-05-03 12:09:10 +0530

    login form validation and userrole redirection

--- StreamlineAcademyWebApp/Sl-Academy-App/src/app/public/Pages/login/login.component.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/public/Pages/login/login.component.ts
@@ -4,35 +4,56 @@ import { Login } from '../../../Models/Common/login';
 import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { HotToastService } from '@ngxpert/hot-toast';
 import { SharedService } from '../../../Services/shared.service';
+import { Router } from '@angular/router';
+import { UserRole } from '../../../Enums/userrole';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private authService: AuthService,
-              private sharedService:SharedService
-  ) { }
+  constructor(
+    private authService: AuthService,
+    private sharedService: SharedService,
+    private router:Router
+  ) {}
   loginModel: Login = new Login();
+  loadSpinner = false;
   loggedIn() {
-    // this.loginModel.email="r@example.com"
-    // this.loginModel.password="r@1234"
+    this.loadSpinner=true;
     this.authService.login(this.loginModel).subscribe({
       next: (response) => {
-        if(response.isSuccess){
-          this.sharedService.showSuccessToast(response.message)
-        }
+        if (response.isSuccess) {
+          this.sharedService.showSuccessToast(response.message);
+          localStorage.setItem("streamlineToken", JSON.stringify(response.result.token));
+        } 
         else {
-          this.sharedService.showErrorToast(response.message)
+          this.sharedService.showErrorToast(response.message);
+        }
+        switch(response.result.userRole){
+          case UserRole.SuperAdmin:
+            this.router.navigate(['/admin/dashboard']);
+            break;
+          case UserRole.AcademyAdmin:
+            this.router.navigate(['/academy/dashboard']);
+            break;
+          case UserRole.Instructor:
+            this.router.navigate(['/instructor/dashboard']);
+            break;
+          case UserRole.Student:
+            this.router.navigate(['/student/dashboard']);
+            break;
+          default:
+            break;
         }
       },
       error: (err: HttpErrorResponse) => {
         if (err.status == HttpStatusCode.BadRequest) {
-          console.log(err.message)
-          this.sharedService.showErrorToast(err.message)
+          console.log(err.message);
+          this.sharedService.showErrorToast(err.message);
         }
-      }
-    })
+      },
+    });
   }
 }
