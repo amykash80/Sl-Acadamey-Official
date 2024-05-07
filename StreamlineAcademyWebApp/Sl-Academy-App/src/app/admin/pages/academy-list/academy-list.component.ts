d5a77bad2c9bf398a7633f30cfe2d5fe99903293
Sushree Sangita Pani <sushree@anterntech.com>
2024-05-07 14:46:46 +0530

    some changes

--- StreamlineAcademyWebApp/Sl-Academy-App/src/app/admin/pages/academy-list/academy-list.component.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/admin/pages/academy-list/academy-list.component.ts
@@ -10,12 +10,8 @@ import { AcademyResponse } from '../../../Models/Academy/Academy';
 })
 export class AcademyListComponent {
   constructor(private academyService:AcademyService){}
-<<<<<<< Updated upstream
   academyList:AcademyResponse[]=[]
   ngOnInit(){
-=======
-  viewAcademy(){
->>>>>>> Stashed changes
     this.academyService.academyList().subscribe({
      next:(response)=>{
        this.academyList = response.result;
@@ -23,11 +19,7 @@ export class AcademyListComponent {
      },
      error:(err:HttpErrorResponse)=>{
       if(err.status==HttpStatusCode.Unauthorized){
-<<<<<<< Updated upstream
         console.log(err.message)
-=======
-        console.log("You are not authorized")
->>>>>>> Stashed changes
       }
      }
     })
