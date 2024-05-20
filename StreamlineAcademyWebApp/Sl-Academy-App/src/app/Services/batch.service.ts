f8685f069ce6ac4ca0ad4d9871a36443127d7156
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-05-20 12:48:53 +0530

    batch created

--- StreamlineAcademyWebApp/Sl-Academy-App/src/app/Services/batch.service.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/Services/batch.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../enviroments/enviroment';
+import { ApiResponse } from '../Models/Common/api-response';
+import { Observable } from 'rxjs';
+import { BatchRequestModel, BatchResponseModel } from '../Models/Batch/Batch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BatchService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  baseUrl:string =environment.apiUrl
+  createBatch(courseModel:BatchRequestModel):Observable<ApiResponse<BatchResponseModel>>{
+    return this.http.post<ApiResponse<BatchResponseModel>>(this.baseUrl + "Batch/createBatch",courseModel)
+  }
 }
--- StreamlineAcademyWebApp/Sl-Academy-App/src/app/academy/Pages/create-batch/create-batch.component.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/academy/Pages/create-batch/create-batch.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BatchRequestModel } from '../../../Models/Batch/Batch';
+import { ActivatedRoute, Router } from '@angular/router';
+import { InstructorResponseModel } from '../../../Models/Instructor/Instructor';
+import { LocationResponseModel } from '../../../Models/Location/Location';
+import { InstructorService } from '../../../Services/instructor.service';
+import { LocationService } from '../../../Services/location.service';
+import { BatchService } from '../../../Services/batch.service';
+import { SharedService } from '../../../Services/shared.service';
+import { HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 
 @Component({
   selector: 'app-create-batch',
   templateUrl: './create-batch.component.html',
-  styleUrl: './create-batch.component.css'
+  styleUrl: './create-batch.component.css',
 })
 export class CreateBatchComponent {
+  activatedRoute = inject(ActivatedRoute);
+  instructorService = inject(InstructorService);
+  locationService = inject(LocationService);
+  batchService=inject(BatchService)
+  sharedService=inject(SharedService)
+  router=inject(Router)
+  courseId: string = '';
+  constructor() {
+    this.activatedRoute.params.subscribe((paramVal) => {
+      this.courseId = paramVal['courseId'];
+    });
+  }
+  ngOnInit() {
+    this.getAllInstructors();
+    this.getAllLocations();
+  }
+  getAllInstructors() {
+    this.instructorService.instructorList().subscribe((res) => {
+      this.instructors = res.result;
+      console.log(this.instructors)
+    });
+  }
+  getAllLocations() {
+    this.locationService.getAllLocations().subscribe((res) => {
+      this.locations = res.result;
+      console.log(this.locations)
 
+    });
+  }
+  batchModel: BatchRequestModel = new BatchRequestModel();
+  instructors: InstructorResponseModel[] = [];
+  locations: LocationResponseModel[] = [];
+  addBatch() {
+    this.batchModel.courseId=this.courseId
+  this.batchService.createBatch(this.batchModel).subscribe({
+      next: (response) => {
+        console.log(response)
+        if (response.isSuccess) {
+          this.sharedService.showSuccessToast(response.message);
+          // this.router.navigate(['/academy/course-list'])
+        }
+        else{
+          this.sharedService.showErrorToast(response.message)
+        }
+      },
+      error: (err: HttpErrorResponse) => {
+        if (err.status == HttpStatusCode.BadRequest) {
+          console.log(err.message)
+        }
+      }
+    });
+  }
 }
