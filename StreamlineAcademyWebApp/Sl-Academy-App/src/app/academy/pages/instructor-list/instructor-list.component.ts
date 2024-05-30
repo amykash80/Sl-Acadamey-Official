e0849ba8a76e9242e6188c708de2a6f0178c5f2f
Sushree Sangita Pani <sushree@anterntech.com>
2024-05-30 12:18:14 +0530

    instructor search list added

--- StreamlineAcademyWebApp/Sl-Academy-App/src/app/academy/pages/instructor-list/instructor-list.component.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/academy/pages/instructor-list/instructor-list.component.ts
@@ -19,11 +19,56 @@ export class InstructorListComponent {
   }
   instructorList: InstructorResponseModel[] = [];
   searchText=''
-  filterInstructors(){}
+  filteredInstructorList: InstructorResponseModel[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 2;
+  totalItems: number = 0;
+  pages: number[] = [];
+  displayedInstructorList: InstructorResponseModel[] = [];
+
+  filterInstructors(): void {
+    if (!this.searchText.trim()) {
+      this.filteredInstructorList = this.instructorList.slice();
+    } else {
+      const searchTerm = this.searchText.toLowerCase();
+      this.filteredInstructorList = this.instructorList.filter(
+        (instructor) =>
+          instructor.name!.toLowerCase().startsWith(searchTerm) ||
+        instructor.address!.toLowerCase().startsWith(searchTerm) ||
+        instructor.email!.toLowerCase().startsWith(searchTerm) ||
+        instructor.phoneNumber!.toString().toLowerCase().startsWith(searchTerm)
+      );
+    }
+  
+    // Reset pagination to the first page after filtering
+    this.totalItems = this.filteredInstructorList.length;
+    this.currentPage = 1;
+    this.updatePagination();
+  }
+  updatePagination(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = Math.min(startIndex + this.itemsPerPage, this.totalItems);
+    this.displayedInstructorList = this.filteredInstructorList.slice(startIndex, endIndex);
+    this.pages = Array(Math.ceil(this.totalItems / this.itemsPerPage))
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+  
+  goToPage(page: number): void {
+    if (page < 1 || page > this.pages.length) {
+      return;
+    }
+    this.currentPage = page;
+    this.updatePagination();
+  }
+  
   loadAllInstructors() {
     this.instructorService.instructorList().subscribe({
       next: (response) => {
         this.instructorList = response.result;
+        this.totalItems = this.filteredInstructorList.length;
+      this.currentPage = 1; 
+      this.updatePagination();
         console.log(this.instructorList);
       },
       error: (err: HttpErrorResponse) => {
