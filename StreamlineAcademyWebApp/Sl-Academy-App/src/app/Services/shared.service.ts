fe09a7aeed1f42f5f5610962537efa3310c5a9b2
Mir Saheem Shafi <100963202+saheemshafi@users.noreply.github.com>
2024-06-22 19:28:08 +0530

    chore: open/toggle sidebar, fix css in instructor component and header avatar

--- StreamlineAcademyWebApp/Sl-Academy-App/src/app/Services/shared.service.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/app/Services/shared.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngxpert/hot-toast';
 import Swal from 'sweetalert2';
@@ -15,6 +15,11 @@ import { Subject } from 'rxjs';
 export class SharedService {
   private profilePictureChangedSource = new Subject<string>();
   profilePictureChanged$ = this.profilePictureChangedSource.asObservable();
+  mobileSidebarOpen = signal(false);
+
+  toggleSidebar() {
+    this.mobileSidebarOpen.update((v) => !v);
+  }
 
   broadcastProfilePictureChange(filePath: string) {
     this.profilePictureChangedSource.next(filePath);
@@ -22,29 +27,27 @@ export class SharedService {
   constructor() {}
   toast = inject(HotToastService);
   router = inject(Router);
-  academyService=inject(AcademyService);
-  enquiryService=inject(EnquiryService);
-  academyList:AcademyResponse[]=[];
-  enquiryList:EnquiryResponse[]=[];
+  academyService = inject(AcademyService);
+  enquiryService = inject(EnquiryService);
+  academyList: AcademyResponse[] = [];
+  enquiryList: EnquiryResponse[] = [];
 
   getToken(): string {
     return localStorage.getItem('streamlineToken')
       ? JSON.parse(localStorage['streamlineToken'])
       : '';
   }
-  getAllAcademies():AcademyResponse[]{
+  getAllAcademies(): AcademyResponse[] {
     this.academyService.academyList().subscribe((response) => {
-      this.academyList=response.result;
+      this.academyList = response.result;
     });
-    return this.academyList
-
+    return this.academyList;
   }
-  getAllEnquiries():EnquiryResponse[]{
+  getAllEnquiries(): EnquiryResponse[] {
     this.enquiryService.enquiryList().subscribe((response) => {
-      this.enquiryList=response.result;
+      this.enquiryList = response.result;
     });
-    return this.enquiryList
-
+    return this.enquiryList;
   }
   showSuccessToast(message: string) {
     this.toast.success(message);
@@ -55,9 +58,8 @@ export class SharedService {
   NoDataSwal(message: string) {
     Swal.fire({
       text: message,
-       heightAuto: false, 
-    width: '300px'
-    
+      heightAuto: false,
+      width: '300px',
     });
   }
   fireConfirmSwal(message: string) {
@@ -77,41 +79,39 @@ export class SharedService {
       },
     });
   }
-updateConfirmSwal(){
-  return Swal.fire({
-    title: "Do you want to save the changes?",
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: "Save",
-    denyButtonText: `Don't save`
-  }).then((result) => {
-    /* Read more about isConfirmed, isDenied below */
-    if (result.isConfirmed) {
-      Swal.fire("Saved!", "", "success");
-    } else if (result.isDenied) {
-      Swal.fire("Changes are not saved", "", "info");
-    }
-  });
-}
+  updateConfirmSwal() {
+    return Swal.fire({
+      title: 'Do you want to save the changes?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Save',
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Saved!', '', 'success');
+      } else if (result.isDenied) {
+        Swal.fire('Changes are not saved', '', 'info');
+      }
+    });
+  }
   showLoader() {
     this.toast.loading('Logging IN');
   }
   logOutUser() {
     Swal.fire({
-      title: "Are you sure?",
+      title: 'Are you sure?',
       text: "You won't be able to revert this!",
-      icon: "warning",
+      icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, log out!"
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, log out!',
     }).then((result) => {
       if (result.isConfirmed) {
         localStorage.clear();
         this.router.navigate(['login']);
-       
       }
     });
   }
-  
 }
