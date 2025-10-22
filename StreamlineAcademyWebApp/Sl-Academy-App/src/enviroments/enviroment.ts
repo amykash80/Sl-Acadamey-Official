8fe6020522cc913f0e0d454e7384ca75879a6d54
Abid Naseer <abidrather36@gmail.com>
2025-10-22 17:07:24 +0530

    ev variable updted

--- StreamlineAcademyWebApp/Sl-Academy-App/src/enviroments/enviroment.ts
+++ StreamlineAcademyWebApp/Sl-Academy-App/src/enviroments/enviroment.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
     production: true,
-    apiUrl:'https://localhost:7197/Api/',
+    //apiUrl:'https://localhost:7197/Api/',//
     googleOAuthClientId:"929244190016-j19tv2kt7cmkd3daqccirgm38i4m1es1.apps.googleusercontent.com",
     redirectUrl:"http://localhost:4200/google-auth",
-    // apiUrl: 'https://api.streamlineacademies.com/api/',
+     apiUrl: 'https://api.streamlineacademies.com/api/',
     paypalClientId: 'AVhA2MV0mCHG98oLPPufgRzycmoEZPXbMEwF9fJrbnZKALww9UjN3X4Z3BkshyEKi0SYvfytuQrkDrVE'
   };
